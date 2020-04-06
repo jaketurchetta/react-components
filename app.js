854d7e18afc0fb223450e26b1063cd20ceb84008
Jake Turchetta <14jturchetta@gmail.com>
2020-04-06 14:44:58 -0400

    complete bare min reqs

--- app.js
+++ app.js
@@ -1,1 +1,48 @@
-// TODO
+// var Eggs = () => (
+//   <li>Eggs</li>
+// );
+
+// var Milk = () => (
+//   <li>Milk</li>
+// );
+
+// var item = ['Eggs', 'Milk', 'Butter'];
+
+class GroceryListItem extends React.Component {
+
+  constructor (props) {
+    super(props);
+
+    this.state = {
+      bold: false
+    };
+  }
+
+  onListItemHover() {
+      this.setState(
+        {bold: !this.state.bold}
+      );
+  }
+
+
+  render() {
+    var style = {
+      fontWeight: this.state.bold ? '800' : '200'
+    };
+
+    return <li style={style} onMouseOver={this.onListItemHover.bind(this)} onMouseOut={this.onListItemHover.bind(this)}>{this.props.item}</li>;
+  }
+
+}
+
+var GroceryList = (props) => (
+  <ul>
+    {
+      props.items.map( (item) => (
+      <GroceryListItem item={item}/>
+    ))
+    }
+  </ul>
+);
+
+ReactDOM.render(<GroceryList items={['Eggs', 'Milk', 'Butter']}/>, document.getElementById("app"));
